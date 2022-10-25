0dd7e1ded980cdf63cf388d36407a1fa4fb0d3d1
Bjalki <braytonalkinburgh@gmail.com>
2022-10-25 12:34:36 -0400

    Pair and cons

--- Pair.java
+++ Pair.java
@@ -1,3 +1,21 @@
-public class Pair {
-    
+import java.util.ArrayList;
+
+public class Pair extends Exp{
+
+    ArrayList<Exp> pair = new ArrayList<>();
+
+    public Pair(Exp e1, Exp e2){
+        pair.add(e1);
+        pair.add(e2);
+    }
+    @Override
+    ValEnv eval(Env env) {
+        
+        return new ValEnv(pair, env); //I don't think this is right
+    }
+    @Override
+    void print() {
+        System.out.println("(" + pair.get(0) + " . " + pair.get(1) + ")");
+
+    }
 }
