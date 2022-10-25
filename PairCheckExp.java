3403d1318cff471b78127830bf91e870ed2c8894
Bjalki <braytonalkinburgh@gmail.com>
2022-10-25 10:03:03 -0400

    MiniScheme5 and PairCheck

---
+++ PairCheckExp.java
@@ -0,0 +1,25 @@
+public class PairCheckExp extends Exp{
+
+    Exp exp;
+
+
+    public PairCheckExp(Exp exp){
+        this.exp = exp;
+    }
+
+    @Override
+    ValEnv eval(Env env) {
+        ValEnv ve = exp.eval(env);
+		Object value = ve.val;
+		boolean result = value instanceof Pair;
+		return new ValEnv(result, ve.env);
+    }
+
+    @Override
+    void print() {
+        System.out.print("(Pair? ");
+		exp.print();
+		System.out.print(")");
+    }
+    
+}
